b00c5e4d2694c70e95e719ed5524dc2e998e036c
testr01 <yvenstest@gmail.com>
2024-09-30 22:25:53 -0400

    ajout implementation et version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.artspace_android"
-        minSdk = 34
+        minSdk = 26 // Recommandé pour une large compatibilité
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.1" // Assurez-vous que cette version correspond à vos bibliothèques Compose
     }
     packaging {
         resources {
@@ -50,15 +50,28 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
+
+    // BOM pour gérer les versions de Compose de manière centralisée
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
+
+    implementation("androidx.compose.material:material:1.2.1")
+
+
     implementation("androidx.compose.material3:material3")
+
+    
+    implementation("androidx.compose.animation:animation")
+
+    // Testing and Debugging Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
